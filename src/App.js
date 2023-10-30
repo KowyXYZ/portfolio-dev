8f4e079cd66bfb537b501b592cec70941a43a005
KowyXYZ <kowybusiness@gmail.com>
2023-10-30 15:03:22 +0100

    start

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import {BrowserRouter} from 'react-router-dom'
+function App() {
+  return (
+    <BrowserRouter>
+      <div className='w-full'>
+
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
